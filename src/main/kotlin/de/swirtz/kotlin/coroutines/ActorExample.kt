b3587fb7a2e8364cecd79fd37842f36cd3e8698a
SIM <simon.wirtz@n-design.de>
2017-07-06 10:28:20 +0200

    minor refactoring of actor example

--- src/main/kotlin/de/swirtz/kotlin/coroutines/ActorExample.kt
+++ src/main/kotlin/de/swirtz/kotlin/coroutines/ActorExample.kt
@@ -9,46 +9,40 @@ import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
 
-/**
- *
- * File created on 05.07.2017.
- */
-
-// Message types for counterActor
-sealed class CounterMsg
-
-object IncCounter : CounterMsg() // one-way message to increment counter
-class GetCounter(val response: SendChannel<Int>) : CounterMsg() // a request with reply
+sealed class CounterMsg {
+    object IncCounter : CounterMsg() // one-way message to increment counter
+    class GetCounter(val response: SendChannel<Int>) : CounterMsg() // a request with channel for reply.
+}
 
 // This function launches a new counter actor
 fun counterActor() = actor<CounterMsg>(CommonPool) {
-    var counter = 0 // actor state
-    for (msg in channel) { // iterate over incoming messages
+    var counter = 0 // actor state, not shared
+    for (msg in channel) { // handle incoming messages
         when (msg) {
-            is IncCounter -> counter++
-            is GetCounter -> msg.response.send(counter)
+            is CounterMsg.IncCounter -> counter++
+            is CounterMsg.GetCounter -> msg.response.send(counter)
         }
     }
 }
 
 suspend fun getCurrentCount(counter: ActorJob<CounterMsg>, response: Channel<Int>): Int {
-    counter.send(GetCounter(response))
+    counter.send(CounterMsg.GetCounter(response))
     val receive = response.receive()
     println("Counter = $receive")
     return receive
 }
 
 fun main(args: Array<String>) = runBlocking<Unit> {
-    val counter = counterActor() // create the actor
+    val counter = counterActor()
     val response = Channel<Int>()
+
     launch(CommonPool) {
             while(getCurrentCount(counter, response) < 100){
                 delay(100)
-                println("sending")
-                counter.send(IncCounter)
+                println("sending IncCounter message")
+                counter.send(CounterMsg.IncCounter)
             }
         }
-    // jobs.forEach{it.join()}
 
     launch(CommonPool) {
         while ( getCurrentCount(counter, response) < 100) {
